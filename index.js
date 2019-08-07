50819c847e526a9f21accd1e0a565c4a09de9a56
Mouni Mullapudi <mullapudi24@gmail.com>
2019-08-07 23:26:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 let driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  // Alternate using ES6 Spread operators:
-  // return { ...driver, ...{ [key]: value } }
   return Object.assign({}, driver, { [key]: value });
 }
+
