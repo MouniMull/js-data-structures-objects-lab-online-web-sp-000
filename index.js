b11ec7a67003ac44f7289df03e0639a0c8233a9d
Mouni Mullapudi <mullapudi24@gmail.com>
2019-08-07 23:26:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Write your solution in this file!
+
+let driver = {}
+
 function updateDriverWithKeyAndValue(driver, key, value) {
+  // Alternate using ES6 Spread operators:
+  // return { ...driver, ...{ [key]: value } }
   return Object.assign({}, driver, { [key]: value });
 }
