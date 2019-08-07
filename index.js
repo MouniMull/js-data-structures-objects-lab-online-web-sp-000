b530ac008ea887bb7b7312cc43aea156f0ddce19
Mouni Mullapudi <mullapudi24@gmail.com>
2019-08-07 23:30:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  return driver.
+  return driver[key] = value
 }
